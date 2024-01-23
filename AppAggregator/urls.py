66c0c2063dc2c24cafa4f0ac847ffbe0330c1ca2
NeerajN10 <neeraj.nair@aceerpsolutions.com>
2024-01-23 19:11:23 +0530

    print statements

--- AppAggregator/urls.py
+++ AppAggregator/urls.py
@@ -33,5 +33,3 @@ urlpatterns = [
 if DEBUG:
     urlpatterns += path("schema/", SpectacularAPIView.as_view(), name="schema"), \
                    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui')
-
-print(urlpatterns)
